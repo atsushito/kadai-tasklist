113f419c599fd01d61d0818e8643bfbebb174a03
EC2 Default User <ec2-user@ip-172-31-5-242.us-east-2.compute.internal>
2018-03-07 10:48:21 +0000

    user login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+
   def index
       @tasks = current_user.tasks.all.page(params[:page])
   end
